11bd93fb8888f2b668c2cf0c7732b331181b4b31
Andrew Yan <ayan@usgs.gov>
2018-08-07 08:28:41 -0500

    correct rollbacks from nldi to postgres users

--- nldi-liquibase/src/main/resources/liquibase/roles.sql
+++ nldi-liquibase/src/main/resources/liquibase/roles.sql
@@ -6,9 +6,9 @@
 create role nhdplus;
 --rollback drop role if exists nhdplus;
 
---changeset drsteini:grant.nhdplus.to.nldi
+--changeset drsteini:grant.nhdplus.to.postgres
 grant nhdplus to postgres;
---rollback revoke nhdplus from nldi;
+--rollback revoke nhdplus from postgres;
 
 --changeset drsteini:create.role.nldi_data
 --preconditions onFail:MARK_RAN onError:HALT
@@ -16,9 +16,9 @@ grant nhdplus to postgres;
 create role nldi_data with login password '${NLDI_DATA_PASSWORD}';
 --rollback drop role if exists nldi_data;
 
---changeset drsteini:grant.nldi_data.to.nldi
+--changeset drsteini:grant.nldi_data.to.postgres
 grant nldi_data to postgres;
---rollback revoke nldi_data from nldi;
+--rollback revoke nldi_data from postgres;
 
 --changeset drsteini:create.role.nldi_user
 --preconditions onFail:MARK_RAN onError:HALT
