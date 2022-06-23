b9eadd0a465594b11899a5e6e8b2a51c704f4b79
Ethan Grahn <egrahn@usgs.gov>
2022-06-23 09:51:32 -0500

    Allow setting additional java opts

--- liquibase/docker-entrypoint.sh
+++ liquibase/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 bash /usr/local/bin/wait-for-it.sh $NLDI_DATABASE_ADDRESS:5432 -- echo "Postgres is up - executing command"
 
+export JAVA_OPTS="${JAVA_OPTS} ${EXTRA_JAVA_OPTS}"
+
 for f in /docker-entrypoint-initdb.d/*; do
 	if [ -x "$f" ]; then
 		echo "$0: running $f"
